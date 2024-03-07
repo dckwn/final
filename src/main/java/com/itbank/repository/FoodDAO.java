fb8b7e4333facb50e6458267b99f4c43ac633eac
dckwn <d_ckwn@nate.com>
2024-03-07 15:31:47 +0900

    ddksajflkdsajfdsafdsa;l

--- src/main/java/com/itbank/repository/FoodDAO.java
+++ src/main/java/com/itbank/repository/FoodDAO.java
@@ -1,5 +1,6 @@
 package com.itbank.repository;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
@@ -18,5 +19,20 @@ public interface FoodDAO {
 
 	@Insert ("insert into health_nutrition (userid, food_idx, meal_time) values (#{userid}, #{food_idx}, #{meal_time})")
 	int insert(NutritionDTO nutritionDto);
+
+	@Select("select * from health_food where food_name like #{food_name} AND group_name like #{group_name}")
+	List<FoodDTO> selectSearchList(HashMap<String, Object> map);
+
+	@Select("SELECT group_name FROM health_food "
+			+ "WHERE group_name IN ('밥류', '구이류', '국류') "
+			+ "GROUP BY group_name "
+			+ "UNION "
+			+ "SELECT group_name FROM ( "
+			+ "SELECT group_name "
+			+ "FROM health_food "
+			+ "WHERE group_name NOT IN ('밥류', '구이류', '국류') "
+			+ "ORDER BY DBMS_RANDOM.VALUE "
+			+ ") WHERE rownum <= 5")
+	List<String> selectCategory();
 
 }
